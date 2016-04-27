9ccc4e3ead6acfcd97fb3538dc880198f38cfec8
eLBati <lorenzo.battistini@agilebg.com>
2016-04-27 18:21:41 +0200

    ADD first draft module with 1 tax line

---
+++ l10n_it_tax_report/__openerp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# © 2016 Lorenzo Battistini - Agile Business Group
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Italian tax report",
+    "summary": "Module summary",
+    "version": "9.0.1.0.0",
+    "category": "Localization",
+    "website": "https://odoo-community.org",
+    "author": "Agile Business Group, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "l10n_it",
+        "mis_builder",
+    ],
+    "data": [
+        "data/mis_report_vat.xml"
+    ],
+}
